af0436c670d2c9a3f579463aef82dbae05c117d5
ducnguyen1911 <ducnguyen1911@gmail.com>
2015-03-16 14:40:29 -0500

    [9.1] Discuss BigInteger and limitation for input

--- src/dynamicprogramming9/RunStaircase1.java
+++ src/dynamicprogramming9/RunStaircase1.java
@@ -1,36 +1,47 @@
 package dynamicprogramming9;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
 public class RunStaircase1 {
 
-	private static Map<Integer, Integer> results;
+	private static Map<Integer, BigInteger> results;
 	private static int numbCall = 0;
 	public static void main(String[] args) {
 		RunStaircase1 rs = new RunStaircase1();
-		results = new HashMap<Integer, Integer>();
-		results.put(0, 1);
-		results.put(1, 1);
-		System.out.println(rs.countSteps(10));
+		results = new HashMap<Integer, BigInteger>();
+		results.put(0, new BigInteger("1"));
+		results.put(1, new BigInteger("1"));
+		System.out.println(rs.countSteps(10000));
 		System.out.println("Total number of calls: " + numbCall);
 	}
 
 	/**
 	 * Implementation with Dynamic Programming
 	 * @param level
-	 * @return
+	 * @return number of ways for running up a staircase
+	 * 
+	 * Note: should limit the max value of level to prevent exceptions?
+	 * Question: what is max value of a BigInteger?
 	 */
-	private int countSteps(int level) {
+	private BigInteger countSteps(int level) {
 		numbCall += 1;
-		System.out.println("call count function: " + level);
+//		System.out.println("call count function: " + level);
 		if (level < 0) {
-			return 0;
+			return new BigInteger("0");
 		} else if (!results.containsKey(level)) {
-			results.put(level, countSteps(level - 1) 
-							 + countSteps(level - 2)
-							 + countSteps(level - 3));
+			results.put(level, countSteps(level - 3)
+					.add(countSteps(level - 2))
+					.add(countSteps(level - 1)));
 		}
 		return results.get(level);
 	}
+	
+	/**
+	 * ANALYSIS: 
+	 * + Memory: O(n)
+	 * + Time: 
+	 */
+	
 }
