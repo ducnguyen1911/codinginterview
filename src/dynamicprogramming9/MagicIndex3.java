08bede1525f0a3887ee7f16ed2ae2c4ad3b93102
ducnguyen1911 <ducnguyen1911@gmail.com>
2015-03-16 16:56:57 -0500

    add 9.4

---
+++ src/dynamicprogramming9/MagicIndex3.java
@@ -0,0 +1,10 @@
+package dynamicprogramming9;
+
+public class MagicIndex3 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/dynamicprogramming9/Subset4.java
@@ -0,0 +1,42 @@
+package dynamicprogramming9;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Subset4 {
+
+	public static void main(String[] args) {
+		Subset4 s = new Subset4();
+		List<String> lst = s.findSubsets(3);
+		System.out.println(lst.size());
+		for (String str : lst) {
+			System.out.println(str);
+		}
+
+	}
+
+	private static Map<Integer, List<String>> results = new HashMap<Integer, List<String>>();
+
+	private List<String> findSubsets(int n){
+		if (results.containsKey(n)){
+			return results.get(n);
+		}
+		else {
+			List<String> ret = new ArrayList<String>();
+			if (n == 1) {
+				ret.add("1");
+			} else {
+				ret.addAll(findSubsets(n-1));
+				ret.add(String.valueOf(n));
+				List<String> temp = findSubsets(n-1);
+				for (String s : temp){
+					ret.add(s + ',' + n);
+				}
+			}
+			results.put(n, ret);
+			return ret;	
+		}
+	}
+}
